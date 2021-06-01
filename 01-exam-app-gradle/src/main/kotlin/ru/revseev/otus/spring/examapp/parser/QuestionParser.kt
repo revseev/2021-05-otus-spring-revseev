efd0623566011ed6758a4261dc3ef6ed47d806f8
18598290 <RYEvseev@sberbank.ru>
2021-06-01 15:16:43 +0300

    ~ метод сервиса, форматирование вопросов и интерактивный вывод в консоль

--- 01-exam-app-gradle/src/main/kotlin/ru/revseev/otus/spring/examapp/parser/QuestionParser.kt
+++
@@ -1,8 +0,0 @@
-package ru.revseev.otus.spring.examapp.parser
-
-import ru.revseev.otus.spring.examapp.domain.Question
-
-interface QuestionParser {
-
-    fun parse(line: String): Question?
-}
--- 01-exam-app-gradle/src/main/kotlin/ru/revseev/otus/spring/examapp/repo/QuestionRepo.kt
+++
@@ -1,3 +0,0 @@
-package ru.revseev.otus.spring.examapp.repo
-
-interface QuestionRepo
